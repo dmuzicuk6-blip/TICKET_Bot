b7c0d3950f8e3ad768facf84ef3eea421e2b6580
dmuzicuk6-blip <dmuzicuk6@gmail.com>
2026-06-23 19:15:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
-@bot.message_handler(
-    func=lambda message: message.text in [
-        "12.07", "13.07", "14.07", "15.07",
-        "17:00", "20:25", "22:30"
-    ]
-)
+@bot.message_handler(func=lambda message: message.text in [
+    "12.07", "13.07", "14.07", "15.07",
+    "17:00", "20:25", "22:30"
+])
 def save_choice(message):
-    if message.text in ["12.07", "13.07", "14.07", "15.07"]:
-        if message.chat.id not in user_data:
-            user_data[message.chat.id] = {}
+    chat_id = message.chat.id
 
-        user_data[message.chat.id]["date"] = message.text
+    if chat_id not in user_data:
+        user_data[chat_id] = {}
+
+    # якщо це дата
+    if message.text in ["12.07", "13.07", "14.07", "15.07"]:
+        user_data[chat_id]["date"] = message.text
 
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         keyboard.row("17:00", "20:25")
@@ -17,7 +18,19 @@ def save_choice(message):
         keyboard.row("⬅️ Назад")
 
         bot.send_message(
-            message.chat.id,
+            chat_id,
+            "Ок, дату збережено. Обери час:",
+            reply_markup=keyboard
+        )
+
+    # якщо це час
+    else:
+        user_data[chat_id]["time"] = message.text
+
+        bot.send_message(
+            chat_id,
+            f"Готово ✅\nДата: {user_data[chat_id].get('date')}\nЧас: {message.text}"
+        )
             f"Дата збережена: {message.text}\nТепер обери час:",
             reply_markup=keyboard
         )

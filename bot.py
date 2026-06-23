eaecd45f4a025a2d5d51651df544c652a7e94c1a
dmuzicuk6-blip <dmuzicuk6@gmail.com>
2026-06-23 18:31:52 +0300

    Implement basic Telegram bot functionality
    Added a Telegram bot that responds to start command and handles user input for time selection.

--- bot.py
+++ bot.py
@@ -1,1 +1,28 @@
-print("TOKEN =", repr(os.getenv("8422214558:AAFDJ8_6NzYIh3xKoplMLHd5gXijW1Rvk2Q")))
+import telebot
+from telebot import types
+
+TOKEN = "8422214558:AAFDJ8_6NzYIh3xKoplMLHd5gXijW1Rvk2Q"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.row("17:00", "20:25")
+    keyboard.row("22:30", "Ввести свій час")
+
+    bot.send_message(
+        message.chat.id,
+        "Привіт! Обери час:",
+        reply_markup=keyboard
+    )
+
+@bot.message_handler(func=lambda message: True)
+def handle(message):
+    if message.text == "Ввести свій час":
+        bot.send_message(message.chat.id, "Напиши час вручну (наприклад 18:45)")
+    else:
+        bot.send_message(message.chat.id, f"Ти вибрала: {message.text}")
+
+print("Bot started")
+bot.infinity_polling()8:AAFDJ8_6NzYIh3xKoplMLHd5gXijW1Rvk2Q

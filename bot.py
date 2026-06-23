cd28ee3d70b5529f0eb0afa5d609f8b7f6e5fb89
dmuzicuk6-blip <dmuzicuk6@gmail.com>
2026-06-23 20:06:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,11 @@
-import requests
+import telebot
 
-def check_uz(from_city, to_city, date):
-    try:
-        url = "https://booking.uz.gov.ua/train_search/"
+TOKEN = "8422214558:AAFDJ8_6NzYIh3xKoplMLHd5gXijW1Rvk2Q"
+bot = telebot.TeleBot(TOKEN)
 
-        payload = {
-            "from": from_city,
-            "to": to_city,
-            "date": date
-        }
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "OK працює")
 
-        headers = {
-            "User-Agent": "Mozilla/5.0"
-        }
-
-        r = requests.post(url, data=payload, headers=headers, timeout=10)
-
-        data = r.json()
-
-        # якщо є поїзди
-        if "value" in data and len(data["value"]) > 0:
-            return True
-
-        return False
-
-    except Exception as e:
-        print("UZ error:", e)
-        return False
+print("Bot started...")
+bot.infinity_polling()
